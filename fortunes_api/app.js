987e03ec42f2c8c5aeb35c4a82324d4ad4157a18
Bailey Pownell <bailey.pownell@gmail.com>
2020-03-25 21:40:07 -0400

    added update method

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -15,22 +15,23 @@ app.get('/fortunes', (req, res) => {
 
 app.get('/fortunes/random', (req, res) => {
   // this gets logged to development console
-  console.log('requesting random fortunes');
-
+  //console.log('requesting random fortunes');
   res.json(fortunes[Math.floor(Math.random() * fortunes.length)])
 })
 
 app.get('/fortunes/:id', (req, res) => {
-  console.log(req.params);
-  if (fortunes.find(f => f.id != req.params.id)) {
-    res.send('No fortune matches your request')
-  }
+  // if (fortunes.find(f => f.id != req.params.id)) {
+  //   res.send('No fortune matches your request')
+  // }
   res.json(fortunes.find(f => f.id == req.params.id));
-})
+});
+
+const writeFortunes = (json) => {
+  fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err))
+}
 
 app.post('/fortunes', (req, res) => {
   // req gives body of data 
-  console.log(req.body);
   const {message, lucky_number, spirit_animal } = req.body;
   const fortunes_ids = fortunes.map(f => f.id);
   const fortune = { 
@@ -42,10 +43,31 @@ app.post('/fortunes', (req, res) => {
 
 const new_fortunes = fortunes.concat(fortune);
 
-  fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err))
+  writeFortunes(new_fortunes)
   res.json(new_fortunes);
 
 });
+
+// update method
+app.put('/fortunes/:id', (req, res) => {
+  const { id } = req.params;
+  //const { message, lucky_number, spirit_animal } = req.body;
+
+  const old_fortune = fortunes.find(f => f.id == id);
+  // console.log(old_fortune)
+  // if (message) old_fortune.message = message;
+  // if (lucky_number) old_fortune.lucky_number = lucky_number;
+  // if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
+
+  ['message', 'lucky_number', 'spirit_animal'].forEach((key) => {
+    if (req.body[key]) old_fortune[key] =  req.body[key];
+  })
+
+  writeFortunes(fortunes);
+
+  // send back new data 
+  res.json(fortunes)
+})
 
 // export app so www file can access it
 module.exports = app;
